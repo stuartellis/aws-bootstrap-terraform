c60831556a8653c4bdca40ac8b8a76ff0bf4e65a
Stuart Ellis <stuart@stuartellis.name>
2021-04-03 14:18:28 +0100

    Add basic template and validation

--- do.sh
+++ do.sh
@@ -3,6 +3,7 @@
 set -euo pipefail
 
 AWS_CLI=aws
+PYTHON_EXE=python3
 
 export TIMESTAMP=$(date +%Y-%m-%dT%H:%M%z)
 
@@ -13,15 +14,22 @@ fi
 
 case $1 in
   info)
+    which $AWS_CLI
+    which $PYTHON_EXE
     $AWS_CLI --version
   ;;
   clean)
    [ -d "bin" ] && rm -r bin 
-   [ -d "log" ] && rm -r log 
+   [ -d "log" ] && rm -r log
+   [ -d ".venv" ] && rm -r .venv  
   ;;
   setup)
     [ -d "bin" ] || mkdir bin
     [ -d "log" ] || mkdir log
+    [ -d ".venv" ] || python3 -m venv .venv
+  ;;
+  run)
+    aws cloudformation validate-template --template-body file://"$PWD"/cloudformation/cfn-tf-backend.yaml > /dev/null
   ;;
   *)
     echo "$1 is not a valid command"
