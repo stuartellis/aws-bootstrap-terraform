bfb176d2ca238e011a410a426f4a06e8ababf690
Stuart Ellis <stuart@stuartellis.name>
2021-04-06 07:35:06 +0100

    Use KMS for S3 bucket

--- README.md
+++ README.md
@@ -8,29 +8,40 @@ Use the *do.sh* script to setup and run this tooling.
 
     ./do.sh info
 
+Customize the project variables in the script before using it.
+
 This script uses two AWS accounts:
 
 - The operations account - The account that hosts IAM users and Terraform backends. Terraform refers to this as the *administrative account*.
 - The managed account - An account that is being managed by the operations account. Terraform refers to this as the *environment account*.
 
-To check the CloudFormation templates, first set your AWS profile to the operations account and region. Then run this command:
+To check the CloudFormation templates, first set your AWS profile to the operations account and region. Next, run this command:
 
     ./do.sh validate
 
-To create a Terraform backend, first set your AWS profile to the operations account and region. Then run this command:
-
-    ./do.sh tf:backend:create
-
-To create an IAM user for Terraform, first set your AWS profile to the operations account and region. Then run this command:
+To create an IAM user for interactive access, first set your AWS profile to the operations account and region. Run this command:
 
     ./do.sh ops:access:create
 
-Subcommands:
+## Terraform Setup
+
+Set your AWS profile to the managed account and region. Run these commands:
+
+    ./do.sh tf:exec:create
+
+Set your AWS profile to the operations account and region. Run these commands:
+
+    ./do.sh tf:kms:create
+    ./do.sh tf:backend:create
+    ./do.sh tf:access:create
+
+## Subcommands
 
 - *ops:access* - Manage IAM users for human operators in the operations account
 - *ops:exec* - Manage IAM role in the managed account
-- *tf:backend* - Manage Terraform backend in the operations account
 - *tf:access* - Manage IAM users for Terraform in the operations account
+- *tf:backend* - Manage Terraform backend in the operations account
 - *tf:exec* - Manage IAM role for Terraform in the managed account
+- *tf:kms* - Manages KMS for the S3 Terraform backend in the operations account
 
 > The valid options are: *create*, *update* and *delete*.
--- do.sh
+++ do.sh
@@ -2,8 +2,7 @@
 
 set -euo pipefail
 
-AWS_CLI=aws
-PYTHON_EXE=python3
+## Project variables
 
 export AWS_PREFIX=stuartellis
 export AWS_OPS_USER_NAME=stuart.ellis
@@ -18,16 +17,22 @@ export AWS_MANAGED_ENV=dev
 export AWS_OPS_ACCOUNT=119559809358
 export AWS_MANAGED_ACCOUNT=333594256635
 
+## Constants
+
+AWS_CLI=aws
+
 export AWS_OPS_ACCESS_CFN=cfn-interactive-ops-access-users.yaml
 export AWS_OPS_EXEC_CFN=cfn-interactive-ops-exec-role.yaml
 
 export AWS_TF_BACKEND_CFN=cfn-tf-backend.yaml
-export AWS_TF_S3_BACKEND_EXPORT="$AWS_PREFIX-$AWS_MANAGED_PROJECT-tf-backend-$AWS_MANAGED_ENV-s3-state-$AWS_REGION"
-export AWS_TF_DDB_BACKEND_EXPORT="$AWS_PREFIX-$AWS_MANAGED_PROJECT-tf-backend-$AWS_MANAGED_ENV-ddb-lock-$AWS_REGION"
+export AWS_TF_KMS_BACKEND_EXPORT="$AWS_PREFIX-$AWS_MANAGED_PROJECT-$AWS_MANAGED_ENV-tf-statekey-kms-key-arn"
+export AWS_TF_S3_BACKEND_EXPORT="$AWS_PREFIX-$AWS_MANAGED_PROJECT-$AWS_MANAGED_ENV-tf-backend-s3-state-arn"
+export AWS_TF_DDB_BACKEND_EXPORT="$AWS_PREFIX-$AWS_MANAGED_PROJECT-$AWS_MANAGED_ENV-tf-backend-ddb-lock-arn"
 
 export AWS_TF_EXEC_CFN=cfn-tf-exec-role.yaml
 export AWS_TF_ACCESS_CFN=cfn-tf-access-users.yaml
-export AWS_TF_EXEC_ROLE_NAME="$AWS_PREFIX-$AWS_MANAGED_PROJECT-tf-exec-$AWS_MANAGED_ENV"
+export AWS_TF_KMS_CFN=cfn-tf-kms-keys.yaml
+export AWS_TF_EXEC_ROLE_ARN="arn:aws:iam::$AWS_MANAGED_ACCOUNT:role/$AWS_PREFIX-$AWS_MANAGED_PROJECT-$AWS_MANAGED_ENV-tf-exec"
 
 if [ ! "${1:-}" ]; then 
   echo "Specify a subcommand."
@@ -37,63 +42,61 @@ fi
 case $1 in
   info)
     which $AWS_CLI
-    which $PYTHON_EXE
     $AWS_CLI --version
   ;;
-  clean)
-    [ -d "bin" ] && rm -r bin 
-    [ -d "log" ] && rm -r log
-    [ -d ".venv" ] && rm -r .venv  
-  ;;
-  setup)
-    [ -d "bin" ] || mkdir bin
-    [ -d "log" ] || mkdir log
-    [ -d ".venv" ] || python3 -m venv .venv
-  ;;
   ops:access:create)
-    aws cloudformation create-stack --capabilities CAPABILITY_NAMED_IAM --stack-name "$AWS_PREFIX-$AWS_OPS_PROJECT-iactive-ops-access-$AWS_OPS_ENV" --template-body file://"$PWD"/cloudformation/$AWS_OPS_ACCESS_CFN --parameters ParameterKey=UserName,ParameterValue="$AWS_OPS_USER_NAME" ParameterKey=UserEmail,ParameterValue="$AWS_OPS_USER_EMAIL" ParameterKey=Prefix,ParameterValue="$AWS_PREFIX" ParameterKey=ProjectName,ParameterValue="$AWS_OPS_PROJECT" ParameterKey=Environment,ParameterValue="$AWS_OPS_ENV" --tags Key=sje:ManagedBy,Value=CLI
+    aws cloudformation create-stack --capabilities CAPABILITY_NAMED_IAM --stack-name "$AWS_PREFIX-$AWS_OPS_PROJECT-$AWS_OPS_ENV-iactive-ops-access" --template-body file://"$PWD"/cloudformation/$AWS_OPS_ACCESS_CFN --parameters ParameterKey=UserName,ParameterValue="$AWS_OPS_USER_NAME" ParameterKey=UserEmail,ParameterValue="$AWS_OPS_USER_EMAIL" ParameterKey=Prefix,ParameterValue="$AWS_PREFIX" ParameterKey=ProjectName,ParameterValue="$AWS_OPS_PROJECT" ParameterKey=Environment,ParameterValue="$AWS_OPS_ENV" --tags Key=sje:ManagedBy,Value=CLI
   ;;
   ops:access:delete)
-    aws cloudformation delete-stack --stack-name "$AWS_PREFIX-$AWS_OPS_PROJECT-iactive-ops-access-$AWS_OPS_ENV"
+    aws cloudformation delete-stack --stack-name "$AWS_PREFIX-$AWS_OPS_PROJECT-$AWS_OPS_ENV-iactive-ops-access"
   ;;
   ops:access:update)
-    aws cloudformation update-stack --capabilities CAPABILITY_NAMED_IAM --stack-name "$AWS_PREFIX-$AWS_OPS_PROJECT-iactive-ops-access-$AWS_OPS_ENV" --template-body file://"$PWD"/cloudformation/$AWS_OPS_ACCESS_CFN --parameters ParameterKey=UserName,ParameterValue="$AWS_OPS_USER_NAME" ParameterKey=UserEmail,ParameterValue="$AWS_OPS_USER_EMAIL" ParameterKey=Prefix,ParameterValue="$AWS_PREFIX" ParameterKey=ProjectName,ParameterValue="$AWS_OPS_PROJECT" ParameterKey=Environment,ParameterValue="$AWS_OPS_ENV" --tags Key=sje:ManagedBy,Value=CLI
+    aws cloudformation update-stack --capabilities CAPABILITY_NAMED_IAM --stack-name "$AWS_PREFIX-$AWS_OPS_PROJECT-$AWS_OPS_ENV-iactive-ops-access" --template-body file://"$PWD"/cloudformation/$AWS_OPS_ACCESS_CFN --parameters ParameterKey=UserName,ParameterValue="$AWS_OPS_USER_NAME" ParameterKey=UserEmail,ParameterValue="$AWS_OPS_USER_EMAIL" ParameterKey=Prefix,ParameterValue="$AWS_PREFIX" ParameterKey=ProjectName,ParameterValue="$AWS_OPS_PROJECT" ParameterKey=Environment,ParameterValue="$AWS_OPS_ENV" --tags Key=sje:ManagedBy,Value=CLI
   ;;
   ops:exec:create)
-    aws cloudformation create-stack --capabilities CAPABILITY_NAMED_IAM --stack-name "$AWS_PREFIX-$AWS_MANAGED_PROJECT-iactive-ops-exec-$AWS_MANAGED_ENV" --template-body file://"$PWD"/cloudformation/$AWS_OPS_EXEC_CFN --parameters ParameterKey=Prefix,ParameterValue="$AWS_PREFIX" ParameterKey=ProjectName,ParameterValue="$AWS_MANAGED_PROJECT" ParameterKey=Environment,ParameterValue="$AWS_MANAGED_ENV" ParameterKey=ManagingAccountID,ParameterValue="$AWS_OPS_ACCOUNT" --tags Key=sje:ManagedBy,Value=CLI
+    aws cloudformation create-stack --capabilities CAPABILITY_NAMED_IAM --stack-name "$AWS_PREFIX-$AWS_MANAGED_PROJECT-$AWS_MANAGED_ENV-iactive-ops-exec" --template-body file://"$PWD"/cloudformation/$AWS_OPS_EXEC_CFN --parameters ParameterKey=Prefix,ParameterValue="$AWS_PREFIX" ParameterKey=ProjectName,ParameterValue="$AWS_MANAGED_PROJECT" ParameterKey=Environment,ParameterValue="$AWS_MANAGED_ENV" ParameterKey=ManagingAccountID,ParameterValue="$AWS_OPS_ACCOUNT" --tags Key=sje:ManagedBy,Value=CLI
   ;;
   ops:exec:delete)
-    aws cloudformation delete-stack --stack-name "$AWS_PREFIX-$AWS_MANAGED_PROJECT-iactive-ops-exec-$AWS_MANAGED_ENV"
+    aws cloudformation delete-stack --stack-name "$AWS_PREFIX-$AWS_MANAGED_PROJECT-$AWS_MANAGED_ENV-iactive-ops-exec"
   ;;
   ops:exec:update)
-    aws cloudformation update-stack --capabilities CAPABILITY_NAMED_IAM --stack-name "$AWS_PREFIX-$AWS_MANAGED_PROJECT-iactive-ops-exec-$AWS_MANAGED_ENV" --template-body file://"$PWD"/cloudformation/$AWS_OPS_EXEC_CFN --parameters ParameterKey=Prefix,ParameterValue="$AWS_PREFIX" ParameterKey=ProjectName,ParameterValue="$AWS_MANAGED_PROJECT" ParameterKey=Environment,ParameterValue="$AWS_MANAGED_ENV" ParameterKey=ManagingAccountID,ParameterValue="$AWS_OPS_ACCOUNT" --tags Key=sje:ManagedBy,Value=CLI
-  ;;
-  tf:backend:create)
-    aws cloudformation create-stack --stack-name "$AWS_PREFIX-$AWS_MANAGED_PROJECT-tf-backend-$AWS_MANAGED_ENV" --template-body file://"$PWD"/cloudformation/$AWS_TF_BACKEND_CFN --parameters ParameterKey=Prefix,ParameterValue="$AWS_PREFIX" ParameterKey=ProjectName,ParameterValue="$AWS_MANAGED_PROJECT" ParameterKey=Environment,ParameterValue="$AWS_MANAGED_ENV" --tags Key=sje:ManagedBy,Value=CLI
-  ;;
-  tf:backend:delete)
-    aws cloudformation delete-stack --stack-name "$AWS_PREFIX-$AWS_MANAGED_PROJECT-tf-backend-$AWS_MANAGED_ENV"
-  ;;
-  tf:backend:update)
-    aws cloudformation update-stack --stack-name "$AWS_PREFIX-$AWS_MANAGED_PROJECT-tf-backend-$AWS_MANAGED_ENV" --template-body file://"$PWD"/cloudformation/$AWS_TF_BACKEND_CFN --parameters ParameterKey=Prefix,ParameterValue="$AWS_PREFIX" ParameterKey=ProjectName,ParameterValue="$AWS_MANAGED_PROJECT" ParameterKey=Environment,ParameterValue="$AWS_MANAGED_ENV" --tags Key=sje:ManagedBy,Value=CLI
+    aws cloudformation update-stack --capabilities CAPABILITY_NAMED_IAM --stack-name "$AWS_PREFIX-$AWS_MANAGED_PROJECT-$AWS_MANAGED_ENV-iactive-ops-exec" --template-body file://"$PWD"/cloudformation/$AWS_OPS_EXEC_CFN --parameters ParameterKey=Prefix,ParameterValue="$AWS_PREFIX" ParameterKey=ProjectName,ParameterValue="$AWS_MANAGED_PROJECT" ParameterKey=Environment,ParameterValue="$AWS_MANAGED_ENV" ParameterKey=ManagingAccountID,ParameterValue="$AWS_OPS_ACCOUNT" --tags Key=sje:ManagedBy,Value=CLI
   ;;
   tf:access:create)
-    aws cloudformation create-stack --capabilities CAPABILITY_NAMED_IAM --stack-name "$AWS_PREFIX-$AWS_MANAGED_PROJECT-tf-access-$AWS_MANAGED_ENV" --template-body file://"$PWD"/cloudformation/$AWS_TF_ACCESS_CFN --parameters ParameterKey=Prefix,ParameterValue="$AWS_PREFIX" ParameterKey=ProjectName,ParameterValue="$AWS_MANAGED_PROJECT" ParameterKey=Environment,ParameterValue="$AWS_MANAGED_ENV" ParameterKey=ManagedAccountID,ParameterValue="$AWS_MANAGED_ACCOUNT" ParameterKey=TfS3BackendImport,ParameterValue="$AWS_TF_S3_BACKEND_EXPORT" ParameterKey=TfDdbBackendImport,ParameterValue="$AWS_TF_DDB_BACKEND_EXPORT" --tags Key=sje:ManagedBy,Value=CLI
+    aws cloudformation create-stack --capabilities CAPABILITY_NAMED_IAM --stack-name "$AWS_PREFIX-$AWS_MANAGED_PROJECT-$AWS_MANAGED_ENV-tf-access" --template-body file://"$PWD"/cloudformation/$AWS_TF_ACCESS_CFN --parameters ParameterKey=Prefix,ParameterValue="$AWS_PREFIX" ParameterKey=ProjectName,ParameterValue="$AWS_MANAGED_PROJECT" ParameterKey=Environment,ParameterValue="$AWS_MANAGED_ENV" ParameterKey=TfExecRoleArn,ParameterValue="$AWS_TF_EXEC_ROLE_ARN" ParameterKey=TfKmsKeyBackendImport ParameterValue="$AWS_TF_KMS_BACKEND_EXPORT" ParameterKey=TfS3BackendImport,ParameterValue="$AWS_TF_S3_BACKEND_EXPORT" ParameterKey=TfDdbBackendImport,ParameterValue="$AWS_TF_DDB_BACKEND_EXPORT" --tags Key=sje:ManagedBy,Value=CLI
   ;;
   tf:access:delete)
-    aws cloudformation delete-stack --stack-name "$AWS_PREFIX-$AWS_MANAGED_PROJECT-tf-access-$AWS_MANAGED_ENV"
+    aws cloudformation delete-stack --stack-name "$AWS_PREFIX-$AWS_MANAGED_PROJECT-$AWS_MANAGED_ENV-tf-access"
   ;;
   tf:access:update)
-    aws cloudformation update-stack --capabilities CAPABILITY_NAMED_IAM --stack-name "$AWS_PREFIX-$AWS_MANAGED_PROJECT-tf-access-$AWS_MANAGED_ENV" --template-body file://"$PWD"/cloudformation/$AWS_TF_ACCESS_CFN --parameters ParameterKey=Prefix,ParameterValue="$AWS_PREFIX" ParameterKey=ProjectName,ParameterValue="$AWS_MANAGED_PROJECT" ParameterKey=Environment,ParameterValue="$AWS_MANAGED_ENV" ParameterKey=ManagedAccountID,ParameterValue="$AWS_MANAGED_ACCOUNT" ParameterKey=TfS3BackendImport,ParameterValue="$AWS_TF_S3_BACKEND_EXPORT" ParameterKey=TfDdbBackendImport,ParameterValue="$AWS_TF_DDB_BACKEND_EXPORT" --tags Key=sje:ManagedBy,Value=CLI
+    aws cloudformation update-stack --capabilities CAPABILITY_NAMED_IAM --stack-name "$AWS_PREFIX-$AWS_MANAGED_PROJECT-$AWS_MANAGED_ENV-tf-access" --template-body file://"$PWD"/cloudformation/$AWS_TF_ACCESS_CFN --parameters ParameterKey=Prefix,ParameterValue="$AWS_PREFIX" ParameterKey=ProjectName,ParameterValue="$AWS_MANAGED_PROJECT" ParameterKey=Environment,ParameterValue="$AWS_MANAGED_ENV" ParameterKey=TfExecRoleArn,ParameterValue="$AWS_TF_EXEC_ROLE_ARN" ParameterKey=TfKmsKeyBackendImport ParameterValue="$AWS_TF_KMS_BACKEND_EXPORT" ParameterKey=TfS3BackendImport,ParameterValue="$AWS_TF_S3_BACKEND_EXPORT" ParameterKey=TfDdbBackendImport,ParameterValue="$AWS_TF_DDB_BACKEND_EXPORT" --tags Key=sje:ManagedBy,Value=CLI
+  ;;
+  tf:backend:create)
+    aws cloudformation create-stack --stack-name "$AWS_PREFIX-$AWS_MANAGED_PROJECT-$AWS_MANAGED_ENV-tf-backend" --template-body file://"$PWD"/cloudformation/$AWS_TF_BACKEND_CFN --parameters ParameterKey=Prefix,ParameterValue="$AWS_PREFIX" ParameterKey=ProjectName,ParameterValue="$AWS_MANAGED_PROJECT" ParameterKey=Environment,ParameterValue="$AWS_MANAGED_ENV" ParameterKey=KmsKeyImport,ParameterValue="$AWS_TF_KMS_BACKEND_EXPORT" --tags Key=sje:ManagedBy,Value=CLI
+  ;;
+  tf:backend:delete)
+    aws cloudformation delete-stack --stack-name "$AWS_PREFIX-$AWS_MANAGED_PROJECT-$AWS_MANAGED_ENV-tf-backend"
+  ;;
+  tf:backend:update)
+    aws cloudformation update-stack --stack-name "$AWS_PREFIX-$AWS_MANAGED_PROJECT-$AWS_MANAGED_ENV-tf-backend" --template-body file://"$PWD"/cloudformation/$AWS_TF_BACKEND_CFN --parameters ParameterKey=Prefix,ParameterValue="$AWS_PREFIX" ParameterKey=ProjectName,ParameterValue="$AWS_MANAGED_PROJECT" ParameterKey=Environment,ParameterValue="$AWS_MANAGED_ENV" ParameterKey=KmsKeyImport,ParameterValue="$AWS_TF_KMS_BACKEND_EXPORT" --tags Key=sje:ManagedBy,Value=CLI
   ;;
   tf:exec:create)
-    aws cloudformation create-stack --capabilities CAPABILITY_NAMED_IAM --stack-name "$AWS_PREFIX-$AWS_MANAGED_PROJECT-tf-exec-$AWS_MANAGED_ENV" --template-body file://"$PWD"/cloudformation/$AWS_TF_EXEC_CFN --parameters ParameterKey=Prefix,ParameterValue="$AWS_PREFIX" ParameterKey=ProjectName,ParameterValue="$AWS_MANAGED_PROJECT" ParameterKey=Environment,ParameterValue="$AWS_MANAGED_ENV" ParameterKey=ManagingAccountID,ParameterValue="$AWS_OPS_ACCOUNT" --tags Key=sje:ManagedBy,Value=CLI
+    aws cloudformation create-stack --capabilities CAPABILITY_NAMED_IAM --stack-name "$AWS_PREFIX-$AWS_MANAGED_PROJECT-$AWS_MANAGED_ENV-tf-exec" --template-body file://"$PWD"/cloudformation/$AWS_TF_EXEC_CFN --parameters ParameterKey=Prefix,ParameterValue="$AWS_PREFIX" ParameterKey=ProjectName,ParameterValue="$AWS_MANAGED_PROJECT" ParameterKey=Environment,ParameterValue="$AWS_MANAGED_ENV" ParameterKey=ManagingAccountID,ParameterValue="$AWS_OPS_ACCOUNT" --tags Key=sje:ManagedBy,Value=CLI
   ;;
   tf:exec:delete)
-    aws cloudformation delete-stack --stack-name "$AWS_PREFIX-$AWS_MANAGED_PROJECT-tf-exec-$AWS_MANAGED_ENV"
+    aws cloudformation delete-stack --stack-name "$AWS_PREFIX-$AWS_MANAGED_PROJECT-$AWS_MANAGED_ENV-tf-exec"
   ;;
   tf:exec:update)
-    aws cloudformation update-stack --capabilities CAPABILITY_NAMED_IAM --stack-name "$AWS_PREFIX-$AWS_MANAGED_PROJECT-tf-exec-$AWS_MANAGED_ENV" --template-body file://"$PWD"/cloudformation/$AWS_TF_EXEC_CFN --parameters ParameterKey=Prefix,ParameterValue="$AWS_PREFIX" ParameterKey=ProjectName,ParameterValue="$AWS_MANAGED_PROJECT" ParameterKey=Environment,ParameterValue="$AWS_MANAGED_ENV" ParameterKey=ManagingAccountID,ParameterValue="$AWS_OPS_ACCOUNT" --tags Key=sje:ManagedBy,Value=CLI
+    aws cloudformation update-stack --capabilities CAPABILITY_NAMED_IAM --stack-name "$AWS_PREFIX-$AWS_MANAGED_PROJECT-$AWS_MANAGED_ENV-tf-exec" --template-body file://"$PWD"/cloudformation/$AWS_TF_EXEC_CFN --parameters ParameterKey=Prefix,ParameterValue="$AWS_PREFIX" ParameterKey=ProjectName,ParameterValue="$AWS_MANAGED_PROJECT" ParameterKey=Environment,ParameterValue="$AWS_MANAGED_ENV" ParameterKey=ManagingAccountID,ParameterValue="$AWS_OPS_ACCOUNT" --tags Key=sje:ManagedBy,Value=CLI
+  ;;
+  tf:kms:create)
+    aws cloudformation create-stack --stack-name "$AWS_PREFIX-$AWS_MANAGED_PROJECT-$AWS_MANAGED_ENV-tf-statekey" --template-body file://"$PWD"/cloudformation/$AWS_TF_KMS_CFN --parameters ParameterKey=Prefix,ParameterValue="$AWS_PREFIX" ParameterKey=ProjectName,ParameterValue="$AWS_MANAGED_PROJECT" ParameterKey=ServiceName,ParameterValue=s3 ParameterKey=Environment,ParameterValue="$AWS_MANAGED_ENV" --tags Key=sje:ManagedBy,Value=CLI
+  ;;
+  tf:kms:delete)
+    aws cloudformation delete-stack --stack-name "$AWS_PREFIX-$AWS_MANAGED_PROJECT-$AWS_MANAGED_ENV-tf-statekey"
+  ;;
+  tf:kms:update)
+    aws cloudformation update-stack --stack-name "$AWS_PREFIX-$AWS_MANAGED_PROJECT-$AWS_MANAGED_ENV-tf-statekey" --template-body file://"$PWD"/cloudformation/$AWS_TF_KMS_CFN --parameters ParameterKey=Prefix,ParameterValue="$AWS_PREFIX" ParameterKey=ProjectName,ParameterValue="$AWS_MANAGED_PROJECT" ParameterKey=ServiceName,ParameterValue=s3 ParameterKey=Environment,ParameterValue="$AWS_MANAGED_ENV" --tags Key=sje:ManagedBy,Value=CLI
   ;;
   validate)
     CFN_TEMPLATES=$(ls "$PWD"/cloudformation/*.yaml)
